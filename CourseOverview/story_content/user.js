d55867c2737da995bbcf72da5513de0d2afc42d5
Siying <sd2888@columbia.edu>
2019-06-10 05:48:54 -0400

    add eLearnings

---
+++ CourseOverview/story_content/user.js
@@ -0,0 +1,81 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6WsRMGJkcvH":
+        Script1();
+        break;
+      case "6WNf3vEqs3o":
+        Script2();
+        break;
+      case "5V5B6CNmH6c":
+        Script3();
+        break;
+      case "6bZCDNsUd5W":
+        Script4();
+        break;
+      case "6dIOnlchqli":
+        Script5();
+        break;
+      case "6DgRtDTF006":
+        Script6();
+        break;
+  }
+}
+
+function Script1()
+{
+  var body = document.querySelector("body")
+var style = document.createElement('style')
+style.type = 'text/css';
+style.appendChild(document.createTextNode(".caption p { background: rgba(207,207,207,0.8); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+body.appendChild(style);
+
+
+}
+
+function Script2()
+{
+  var body = document.querySelector("body")
+var style = document.createElement('style')
+style.type = 'text/css';
+style.appendChild(document.createTextNode(".cs-seekcontrol {border-color:black; border-style:solid; border-width: 0px 0px 0px 0px; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+body.appendChild(style);
+}
+
+function Script3()
+{
+  var body = document.querySelector("body")
+var style = document.createElement('style')
+style.type = 'text/css';
+style.appendChild(document.createTextNode("#tab-outline {border-color:black; border-style:solid; border-width: 2px 2px 2px 2px; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+body.appendChild(style);
+}
+
+function Script4()
+{
+  var body = document.querySelector("body")
+var style = document.createElement('style')
+style.type = 'text/css';
+style.appendChild(document.createTextNode("#tab-outline {border:none; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+body.appendChild(style);
+}
+
+function Script5()
+{
+  var body = document.querySelector("body")
+var style = document.createElement('style')
+style.type = 'text/css';
+style.appendChild(document.createTextNode("#tab-customlink {border-color:black; border-style:solid; border-width: 1px 1px 1px 1px; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+body.appendChild(style);
+}
+
+function Script6()
+{
+  var body = document.querySelector("body")
+var style = document.createElement('style')
+style.type = 'text/css';
+style.appendChild(document.createTextNode("#tab-customlink {border:none; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+body.appendChild(style);
+}
+
---
+++ System - Process Simulation demo/story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6oxiarmZ3TE":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  alert("please fill in your name!");
+}
+
