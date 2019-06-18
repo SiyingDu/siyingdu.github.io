ce461214569f5f505171d49b64b3f0253f22cbe5
Siying <sd2888@columbia.edu>
2019-06-18 04:45:04 -0400

    am topic 1

---
+++ System Training AM/story_content/user.js
@@ -0,0 +1,94 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5pelxfs6DCn":
+        Script1();
+        break;
+      case "5kTOJ1WVfmD":
+        Script2();
+        break;
+      case "5gzkVXDtp3T":
+        Script3();
+        break;
+      case "6Mv7qmjqk0c":
+        Script4();
+        break;
+      case "6gGe34fGikq":
+        Script5();
+        break;
+      case "6bzmYNrC8RG":
+        Script6();
+        break;
+      case "6r4NeJdIq0F":
+        Script7();
+        break;
+  }
+}
+
+function Script1()
+{
+  var body = document.querySelector("body")
+var style = document.createElement('style')
+style.type = 'text/css';
+style.appendChild(document.createTextNode(".caption p { background: rgba(207,207,207,0.8); color: black; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+body.appendChild(style);
+
+
+}
+
+function Script2()
+{
+  var body = document.querySelector("body")
+var style = document.createElement('style')
+style.type = 'text/css';
+style.appendChild(document.createTextNode(".caption { margin-bottom:0px; }"));
+body.appendChild(style);
+
+}
+
+function Script3()
+{
+  var body = document.querySelector("body")
+var style = document.createElement('style')
+style.type = 'text/css';
+style.appendChild(document.createTextNode(".cs-seekcontrol {border-color:black; border-style:solid; border-width: 0px 0px 0px 0px; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+body.appendChild(style);
+}
+
+function Script4()
+{
+  var body = document.querySelector("body")
+var style = document.createElement('style')
+style.type = 'text/css';
+style.appendChild(document.createTextNode("#tab-outline {border-color:black; border-style:solid; border-width: 2px 2px 2px 2px; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+body.appendChild(style);
+}
+
+function Script5()
+{
+  var body = document.querySelector("body")
+var style = document.createElement('style')
+style.type = 'text/css';
+style.appendChild(document.createTextNode("#tab-outline {border:none; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+body.appendChild(style);
+}
+
+function Script6()
+{
+  var body = document.querySelector("body")
+var style = document.createElement('style')
+style.type = 'text/css';
+style.appendChild(document.createTextNode("#tab-customlink {border-color:black; border-style:solid; border-width: 1px 1px 1px 1px; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+body.appendChild(style);
+}
+
+function Script7()
+{
+  var body = document.querySelector("body")
+var style = document.createElement('style')
+style.type = 'text/css';
+style.appendChild(document.createTextNode("#tab-customlink {border:none; text-decoration: none; text-shadow: none; font-weight: bold; }"));
+body.appendChild(style);
+}
+
