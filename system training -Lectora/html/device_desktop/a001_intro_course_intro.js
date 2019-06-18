7b401fae804db32eb6fd1d4afbc02fdcb098f3ef
Siying <sd2888@columbia.edu>
2019-06-18 02:35:40 -0400

    system training lectora folder

---
+++ system training -Lectora/html/device_desktop/a001_intro_course_intro.js
@@ -0,0 +1,12 @@
+DesktopResponsive={"1009":{
+"pageLayer":{"w":1009,"h":662,"bgColor":"#fffaf5"}
+,
+"webwidget122321":{"x":0,"y":21,"w":1008,"h":608,"bOffBottom":0}
+,
+"RCDResetQuestion":function(){
+try{if(window.dragMgr)window.dragMgr.clearDropZones();}catch(e){if(e&&e.message)console.log(e.message);}
+}
+,
+"RCDResultResize":function(){}
+,"preload":[]
+}}
