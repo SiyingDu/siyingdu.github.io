b7bc4cfa64a8cea4e179e4b764958e18a5fbaf22
Siying <sd2888@columbia.edu>
2024-09-19 00:18:10 -0400

    updated insider trading course

--- InsiderTrading/story_content/user.js
+++ InsiderTrading/story_content/user.js
@@ -2,12 +2,6 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6r8HBPnRKxE":
-        Script1();
-        break;
-      case "5XKxuVESgoL":
-        Script2();
-        break;
   }
 }
 
@@ -18,24 +12,4 @@ var object = player.object;
 var addToTimeline = player.addToTimeline;
 var setVar = player.SetVar;
 var getVar = player.GetVar;
-window.Script1 = function()
-{
-  $(".volume").after($("#tab-customlink"));
-$("#tab-customlink").addClass('btn cs-button');
-
-$("#tab-customlink").hide();
-
-
-}
-
-window.Script2 = function()
-{
-  window.print();
-$(".volume").after($("#tab-customlink"));
-$("#tab-customlink").addClass('btn cs-button');
-$("#tab-customlink").hide();
-$("#tab-customlink").show();
-
-}
-
 };
